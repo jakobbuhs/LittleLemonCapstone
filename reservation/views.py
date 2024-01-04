78e37fef5227ce923a43e8e6d5bd35ac95adb2b0
jakobbuhs <143163665+jakobbuhs@users.noreply.github.com>
2024-01-04 19:43:22 +0100

    Delete reservation directory

--- reservation/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def sayHello(request):
-    return HttpResponse('Hello World')
-def index(request):
-    return render(request, 'index.html', {})
